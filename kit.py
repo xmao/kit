d438769f18b904b81bc07dca4ffc248e73592266
Xizeng Mao <xmao1@DOCMM0CL8009519.local>
2015-11-13 16:20:38 -0600

    Make more robust by handling keyboard signal

--- kit.py
+++ kit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-import os,sys
-import shutil,tempfile
+import os
+import shutil
+import tempfile
+import subprocess
 
 if 'KITROOT' in os.environ:
     KITROOT = os.environ['KITROOT']
@@ -10,23 +12,24 @@ else:
         os.path.dirname(__file__), 'scripts')
 
 KIT_TYPES = {
-    'awk'           :  '#!/usr/bin/env awk',
-     # Mathematica 8 is required
-    'mathematica'   :  '#!/usr/bin/env MathematicaScript -script',
-    'perl'          :  '#!/usr/bin/env perl',
-    'python'        :  '#!/usr/bin/env python',
-    'R'             :  '#!/usr/bin/env Rscript',
-    'ruby'          :  '#!/usr/bin/env ruby',
-    'sed'           :  '#!/usr/bin/env sed',
-    'shell'         :  '#!/usr/bin/env sh',
-    }
+    'awk':  '#!/usr/bin/env awk',
+    # Mathematica 8 is required
+    'mathematica':  '#!/usr/bin/env MathematicaScript -script',
+    'perl':  '#!/usr/bin/env perl',
+    'python':  '#!/usr/bin/env python',
+    'R':  '#!/usr/bin/env Rscript',
+    'ruby':  '#!/usr/bin/env ruby',
+    'sed':  '#!/usr/bin/env sed',
+    'shell':  '#!/usr/bin/env sh',
+}
+
 
 def kit_edit(names, args):
     def get_editor():
         return os.getenv('KIT_EDITOR') \
-                or os.getenv('VISUAL') \
-                or os.getenv('EDITOR') \
-                or 'vi'
+            or os.getenv('VISUAL') \
+            or os.getenv('EDITOR') \
+            or 'vi'
 
     p = get_program(names)
     f = tempfile.mkstemp(prefix='kit')
@@ -40,35 +43,39 @@ def kit_edit(names, args):
     if os.path.isfile(f[1]):
         os.system('rm -rf %s' % f[1])
 
+
 def kit_create(names, args):
     path = get_program(names)
     os.system('mkdir -p %s' % os.path.dirname(path))
     os.system('touch %s' % path)
     if '-t' in args:
-        type = args[(args.index('-t')+1)]
+        type = args[(args.index('-t') + 1)]
         if type in KIT_TYPES:
             f = open(path, 'w')
             print >>f, KIT_TYPES[type]
             f.close()
     kit_edit(names, args)
 
+
 def get_program(names):
     return os.path.join(KITROOT, *names)
 
+
 def get_names_and_args(args):
     for i in range(len(args)):
-        if os.path.isfile(get_program(args[:i+1])):
-            return args[:i+1], args[i+1:]
+        if os.path.isfile(get_program(args[:i + 1])):
+            return args[:i + 1], args[i + 1:]
     return args, []
 
 if __name__ == '__main__':
-    import os, sys, re
-    
+    import os
+    import sys
+
     names, args = get_names_and_args(sys.argv[1:])
 
     KIT_METHODS = dict(
-        [(k[4:],v) for k,v in vars().items() if k.startswith('kit_')])
-    
+        [(k[4:], v) for k, v in vars().items() if k.startswith('kit_')])
+
     if '-h' in args:
         for l in file(get_program(names)):
             l = l.strip()
@@ -91,4 +98,13 @@ if __name__ == '__main__':
     elif names[0] in KIT_METHODS:
         KIT_METHODS[names[0]](names[1:], args)
     else:
-        exit(os.system('%s %s' % (get_program(names), ' '.join(args))))
+        try:
+            p = subprocess.Popen([get_program(names)] + args)
+            retcode = p.wait()
+        except KeyboardInterrupt, e:
+            p.kill()
+            exit(1)
+        except:
+            raise
+        else:
+            exit(retcode)
